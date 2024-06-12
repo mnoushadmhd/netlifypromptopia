69763dd190d67a24f05d3cb8188ed7aff22d3365
Noushad M <noushad.m@maxwellgeosystems.com>
2024-06-12 21:43:33 +0530

    data fetch check

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -44,13 +44,13 @@ const Feed = () => {
   const PromptCardList=({data,handleTagClick})=>{
     return(
       <div className='mt-16 prompt_layout'>
-      {data.length > 0 ?(data.map((post) => (
+      {/* {data.length > 0 ?(data.map((post) => (
                 <PromptCard 
                     key={post._id}
                     post={post}
                     handleTagClick={handleTagClick}
                 />
-            ))):""}
+            ))):""} */}
       </div>
     )
   }
